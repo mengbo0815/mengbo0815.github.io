dba1c81e396a3352a8d5c1319b213404f8bc43d6
wang mengbo <2183147502@qq.com>
2026-07-07 08:11:08 +0800

    更新网站配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://你的用户名.github.io',
+  site: 'https://mengbo0815.github.io',
   base: '/',
 });
